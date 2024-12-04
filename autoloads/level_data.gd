2c651929ed9889f4e7b9cb98765d1ee4f943750c
justknick <126913469+justknick@users.noreply.github.com>
2024-12-04 15:03:31 -0500

    Section 185.05 - Level Data Setup //

--- autoloads/level_data.gd
+++ autoloads/level_data.gd
@@ -5,11 +5,22 @@ const TILE_SIZE: int = 32
 
 var _level_data: Dictionary = {} 
 # _level_data dictionary will have: level as key, LevelLayout class -> level: LevelLayout
+# use "level" as string
 
 
 func _ready() -> void:
 	load_level_data()
 	pass
+
+
+func setup_level(level_number_string_key: String, raw_level_data: Dictionary) -> LevelLayout: 
+	var layout: LevelLayout = LevelLayout.new()
+	
+	var layout_player_start = raw_level_data.player_start
+	#layout.set_player_start(layout_player_start["x"], layout_player_start["y"])
+	layout.set_player_start(layout_player_start.x, layout_player_start.y)
+	
+	return layout
 
 
 #load out json, iterate through it
@@ -18,4 +29,13 @@ func load_level_data() -> void:
 	var raw_data = JSON.parse_string(file.get_as_text())
 	
 	for level_number_string in raw_data.keys():
-		_level_data[level_number_string] = level_number_string + "_value"
+		
+		#var new_level_layout: LevelLayout = setup_level(level_number_string, raw_data[level_number_string])
+		#print("new level layout: %s %d, %d" % [
+			#new_level_layout.get_instance_id(), 
+			#new_level_layout.get_player_start().x, 
+			#new_level_layout.get_player_start().y
+		#])
+		
+		#_level_data[level_number_string] = level_number_string + "_value"
+		_level_data[level_number_string] = setup_level(level_number_string, raw_data[level_number_string])
