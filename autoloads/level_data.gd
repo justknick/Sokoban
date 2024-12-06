f3fca4735be43c8f318cdb6bba537686fda6d888
justknick <126913469+justknick@users.noreply.github.com>
2024-12-05 23:01:10 -0500

    Section 186.06 - Level Data Processing (cont) //

--- autoloads/level_data.gd
+++ autoloads/level_data.gd
@@ -13,10 +13,28 @@ func _ready() -> void:
 	pass
 
 
+func add_tiles_for_layer(layout: LevelLayout, layer_type: TileLayers.LayerType, tile_coords: Array) -> void:
+	for tile_coord in tile_coords:
+		layout.add_tile_to_layer(tile_coord.x, tile_coord.y, layer_type)
+
+
 func setup_level(level_number_string_key: String, raw_level_data: Dictionary) -> LevelLayout: 
 	var layout: LevelLayout = LevelLayout.new()
 	
+	var raw_tiles: Dictionary = raw_level_data.tiles
+	
 	var layout_player_start = raw_level_data.player_start
+	
+	# godot style would want to invoke method after declaration, but we'll do it here 
+	
+	#FLOOR, WALL, TARGET, BOX, TARGET_BOX
+	add_tiles_for_layer(layout, TileLayers.LayerType.FLOOR, raw_tiles.Floor)
+	add_tiles_for_layer(layout, TileLayers.LayerType.WALL, raw_tiles.Walls)
+	add_tiles_for_layer(layout, TileLayers.LayerType.TARGET, raw_tiles.Targets)
+	add_tiles_for_layer(layout, TileLayers.LayerType.BOX, raw_tiles.Boxes)
+	add_tiles_for_layer(layout, TileLayers.LayerType.TARGET_BOX, raw_tiles.TargetBoxes)
+	
+	
 	#layout.set_player_start(layout_player_start["x"], layout_player_start["y"])
 	layout.set_player_start(layout_player_start.x, layout_player_start.y)
 	
