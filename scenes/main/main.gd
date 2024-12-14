404f568afbcd57df62db92db88a8f453cab5d500
justknick <126913469+justknick@users.noreply.github.com>
2024-12-14 17:07:37 -0500

    Extra - Alternative Button //

--- scenes/main/main.gd
+++ scenes/main/main.gd
@@ -1,6 +1,7 @@
 extends Control
 
 const LEVEL_BUTTON = preload("res://scenes/level_button/level_button.tscn")
+const LEVEL_BUTTON_ALT = preload("res://scenes/level_button/level_button_alt.tscn")
 const LEVEL_COLUMNS: int = 6
 const LEVEL_ROWS: int = 5 
 
@@ -12,6 +13,7 @@ func _ready() -> void:
 
 func setup_grid() -> void: 
 	for level in range(LEVEL_COLUMNS * LEVEL_ROWS):
-		var level_button: LevelButton = LEVEL_BUTTON.instantiate()
+		#var level_button: LevelButton = LEVEL_BUTTON.instantiate()
+		var level_button: LevelButtonAlt = LEVEL_BUTTON_ALT.instantiate()
 		level_button.set_level_number(str(level + 1))
 		level_buttons_container.add_child(level_button)
